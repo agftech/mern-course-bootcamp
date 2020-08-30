dac6c06fbd261134a16d53b9e44d5cdb91931fcf
agftech <agiraof@gmail.com>
2020-08-30 14:49:17 -0300

    :package: feat: add rule to create user on mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+const UserController = require("./controllers/UserController");
 
 const PORT = process.env.port || 8000;
 
@@ -13,21 +14,26 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 app.get("/", (req, res) => {
-	res.send("Hello from express!");
+	res.send("Hello from express \n");
 });
 
 app.get("/register", (req, res) => {
 	res.send("Welcome to Register \n");
 });
 
+app.post("/register", UserController.store);
+
 try {
 	mongoose.connect(process.env.MONGO_DB_SECRET, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
 	});
 	console.log("MongoDb connected successfully!");
 } catch (error) {
 	console.log(error);
+	process.exit();
 }
 
 app.listen(PORT, () => {
