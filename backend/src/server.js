1385c3ea714c26b57c359149a155556ec67bfe50
agftech <agiraof@gmail.com>
2020-08-30 12:15:34 -0300

    :package: feat: add connection rules with mongodb

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
@@ -7,9 +8,23 @@ const PORT = process.env.port || 8000;
 app.use(cors());
 app.use(express.json());
 
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+
 app.get("/", (req, res) => {
 	res.send("Hello from express!");
 });
+
+try {
+	mongoose.connect(process.env.MONGO_DB_SECRET, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+	console.log("MongoDb connected successfully!");
+} catch (error) {
+	console.log(error);
+}
 
 app.listen(PORT, () => {
 	console.log(`Listening on ${PORT}`);
