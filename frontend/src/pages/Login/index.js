66c8c14436a6ba24fed1686233d16ddb81df4e01
agftech <agiraof@gmail.com>
2020-09-08 14:24:07 -0300

    :dizzy: style: add style in Login

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,55 +1,83 @@
 import React, { useState } from 'react';
 import api from '../../services/api';
-import { Container, Button, Form, FormGroup, Input } from 'reactstrap';
+import { Container, Button, Form, FormGroup, Input, Alert } from 'reactstrap';
 
 import './styles.css';
 
 export default function Login({ history }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
 
   const handleSubmit = async (evt) => {
     evt.preventDefault();
-    console.log('result of the submit', email, password);
-
+    //console.log('result of the submit', email, password);
     const response = await api.post('/login', { email, password });
     const userId = response.data._id || false;
 
-    if (userId) {
-      localStorage.setItem('user', userId);
-      history.push('/dashboard');
-    } else {
-      const { message } = response.data;
-      console.log(message);
-    }
+    try {
+      if (userId) {
+        localStorage.setItem('user', userId);
+        history.push('/dashboard');
+      } else {
+        const { message } = response.data;
+        setError(true);
+        setErrorMessage(message);
+        setTimeout(() => {
+          setError(false);
+          setErrorMessage('');
+        }, 2000);
+        //console.log(message);
+      }
+    } catch (error) {}
   };
 
   return (
     <Container>
+      <h2>Login:</h2>
       <p>
         Please <strong>Login</strong> into your account
       </p>
       <Form onSubmit={handleSubmit}>
-        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-          <Input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Your email"
-            onChange={(evt) => setEmail(evt.target.value)}
-          />
+        <div className="input-group">
+          <FormGroup className="form-group-"></FormGroup>
+          <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+            <Input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Your email"
+              onChange={(evt) => setEmail(evt.target.value)}
+            />
+          </FormGroup>
+          <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+            <Input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Your password"
+              onChange={(evt) => setPassword(evt.target.value)}
+            />
+          </FormGroup>
+        </div>
+        <FormGroup>
+          <Button className="submit-btn">Submit</Button>
         </FormGroup>
-        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-          <Input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Your password"
-            onChange={(evt) => setPassword(evt.target.value)}
-          />
+        <FormGroup>
+          <Button className="secondary-btn" onClick={() => history.push('/register')}>
+            New Account
+          </Button>
         </FormGroup>
-        <Button>Submit</Button>
       </Form>
+      {error && errorMessage ? (
+        <Alert className="event-validation" color="danger">
+          {' '}
+          Missing required information
+        </Alert>
+      ) : (
+        ''
+      )}
     </Container>
   );
 }
