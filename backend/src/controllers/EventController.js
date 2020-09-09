a4bebe8716bec102e541daa0c4c8ed6295076c14
agftech <agiraof@gmail.com>
2020-09-09 17:40:17 -0300

    :package: feat: add datefield when creating the sporting event

--- backend/src/controllers/EventController.js
+++ backend/src/controllers/EventController.js
@@ -3,7 +3,7 @@ const User = require("../models/User");
 
 module.exports = {
 	async createEvent(req, res) {
-		const { title, description, price, sport } = req.body;
+		const { title, description, price, sport, date } = req.body;
 		const { user_id } = req.headers;
 		const { filename } = req.file;
 
@@ -20,6 +20,7 @@ module.exports = {
 			price: parseFloat(price),
 			user: user_id,
 			thumbnail: filename,
+			date,
 		});
 
 		return res.json(event);
