7940c3e97ade10243de2eda5d994913ac1dd5a13
agftech <agiraof@gmail.com>
2020-09-10 15:25:59 -0300

    :package: feat: add myevents filter, delete function and validation

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, ButtonGroup } from 'reactstrap';
+import { Button, ButtonGroup, Alert } from 'reactstrap';
 import moment from 'moment';
 
 import api from '../../services/api';
@@ -9,8 +9,9 @@ import './styles.css';
 export default function Dashboard({ history }) {
   const [events, setEvents] = useState([]);
   const user_id = localStorage.getItem('user');
-  //const [cSelected, setCSelected] = useState([]);
   const [rSelected, setRSelected] = useState(null);
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   useEffect(() => {
     getEvents();
@@ -21,6 +22,12 @@ export default function Dashboard({ history }) {
     getEvents(query);
   };
 
+  const myEventsHandler = async () => {
+    setRSelected('myevents');
+    const response = await api.get('/user/events', { headers: { user_id } });
+    setEvents(response.data);
+  };
+
   const getEvents = async (filter) => {
     const url = filter ? `/dashboard/${filter}` : '/dashboard';
     const response = await api.get(url, { headers: { user_id } });
@@ -28,13 +35,31 @@ export default function Dashboard({ history }) {
     setEvents(response.data);
   };
 
+  const deleteEventHandler = async (eventId) => {
+    try {
+      await api.delete(`/event/${eventId}`);
+      setSuccess(true);
+      setTimeout(() => {
+        setSuccess(false);
+        filterHandler(null);
+      }, 2500);
+    } catch (error) {
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 2000);
+    }
+  };
+
   return (
     <>
-      <div>
-        <strong>Filter: </strong>
+      <div className="filter-panel">
         <ButtonGroup>
           <Button color="primary" onClick={() => filterHandler(null)} active={rSelected === null}>
             All Sports
+          </Button>
+          <Button color="primary" onClick={myEventsHandler} active={rSelected === 'myevents'}>
+            My Events
           </Button>
           <Button
             color="primary"
@@ -64,11 +89,20 @@ export default function Dashboard({ history }) {
           </Button>
         </ButtonGroup>
       </div>
-      <br></br>
       <ul className="events-list">
         {events.map((event) => (
           <li key={event._id}>
-            <header style={{ backgroundImage: `url(${event.thumbnail_url})` }} />
+            <header style={{ backgroundImage: `url(${event.thumbnail_url})` }}>
+              {event.user === user_id ? (
+                <div>
+                  <Button color="danger" size="sm" onClick={() => deleteEventHandler(event._id)}>
+                    Delete
+                  </Button>
+                </div>
+              ) : (
+                ''
+              )}
+            </header>
             <strong>{event.title}</strong>
             <span>Event Date: {moment(event.date).format('l')}</span>
             <span>Event Price: {parseFloat(event.price).toFixed(2)}</span>
@@ -78,6 +112,22 @@ export default function Dashboard({ history }) {
           </li>
         ))}
       </ul>
+      {error ? (
+        <Alert className="event-validation" color="danger">
+          {' '}
+          Error when deleting event!{' '}
+        </Alert>
+      ) : (
+        ''
+      )}
+      {success ? (
+        <Alert className="event-validation" color="success">
+          {' '}
+          The event was deleted successfully!
+        </Alert>
+      ) : (
+        ''
+      )}
     </>
   );
 }
