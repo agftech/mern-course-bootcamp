5d3b4dfd517a70dc7d8e708ac65d68270801c9f2
agftech <agiraof@gmail.com>
2020-09-11 12:38:13 -0300

    :package: feat: add websockets

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const routes = require('./routes')
 const path = require('path')
-const app = express()
+const http = require('http')
+const socketio = require('socket.io')
 const PORT = process.env.port || 8000
 
-app.use(cors())
-app.use(express.json())
+const app = express()
+const server = http.Server(app)
+const io = socketio(server)
 
 if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config()
@@ -24,9 +26,23 @@ try {
 	process.exit()
 }
 
+const connectUsers = {}
+io.on('connection', (socket) => {
+	const { user } = socket.handshake.query
+	connectUsers[user] = socket.id
+	console.log('User is connect', socket.id)
+})
+
+app.use((req, res, next) => {
+	req.io = io
+	req.connectUsers = connectUsers
+	return next()
+})
+app.use(cors())
+app.use(express.json())
 app.use('/files', express.static(path.resolve(__dirname, '..', 'files')))
 app.use(routes)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
 	console.log(`Listening on ${PORT}`)
 })
