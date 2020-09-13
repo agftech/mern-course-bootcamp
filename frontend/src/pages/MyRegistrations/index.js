15fe663448756c7e36bbe15be6f44e4481eef71f
agftech <agiraof@gmail.com>
2020-09-13 14:02:51 -0300

    :package: feat: add listing of registered sporting events

--- frontend/src/pages/MyRegistrations/index.js
+++ frontend/src/pages/MyRegistrations/index.js
@@ -1,5 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import { Button, ButtonGroup } from 'reactstrap';
+
+import api from '../../services/api';
 
 export default function MyRegistrations() {
-  return <div>My Events Registrations</div>;
+  const [myEvents, setMyEvents] = useState([]);
+  const user = localStorage.getItem('user');
+
+  useEffect(() => {
+    getMyEvents();
+  }, []);
+
+  const getMyEvents = async () => {
+    try {
+      const response = await api.get('/registration', { headers: { user } });
+      setMyEvents(response.data);
+    } catch (error) {}
+  };
+
+  return (
+    <ul>
+      {myEvents.map((event) => (
+        <li key={event._id}>
+          <div>
+            <strong>{event.eventTitle}</strong>
+          </div>
+          <div>
+            <span>Event Date: {moment(event.eventDate).format('l')}</span>
+            <span>Event Price: ${parseFloat(event.eventPrice).toFixed(2)}</span>
+            <span>User Email: {event.userEmail}</span>
+            <span>Status:</span>
+          </div>
+          <ButtonGroup>
+            <Button
+              disabled={event.approved === true || event.approved === false ? true : false}
+              color="secondary"
+              onClick={() => {}}
+            >
+              Accept
+            </Button>
+            <Button
+              disabled={event.approved === true || event.approved === false ? true : false}
+              color="danger"
+              onClick={() => {}}
+            >
+              Reject
+            </Button>
+          </ButtonGroup>
+        </li>
+      ))}
+    </ul>
+  );
 }
