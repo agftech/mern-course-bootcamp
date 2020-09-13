4e4f3ea5944ffabb0bee9d9899db866e280f2430
agftech <agiraof@gmail.com>
2020-09-13 12:12:25 -0300

    :package: feat: add fields in the record create method in an event

--- backend/src/controllers/RegistrationController.js
+++ backend/src/controllers/RegistrationController.js
@@ -1,50 +1,55 @@
-const Registration = require('../models/Registration')
-const jwt = require('jsonwebtoken')
+const Registration = require("../models/Registration");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
 	create(req, res) {
-		jwt.verify(req.token, 'secret', async (err, authData) => {
+		jwt.verify(req.token, "secret", async (err, authData) => {
 			if (err) {
-				res.sendStatus(401)
+				res.sendStatus(401);
 			} else {
-				const user_id = authData.user._id
-				const { eventId } = req.params
+				const user_id = authData.user._id;
+				const { eventId } = req.params;
 
 				const registration = await Registration.create({
 					user: user_id,
 					event: eventId,
-				})
+				});
 
 				await registration
-					.populate('event')
-					.populate('user', '-password')
-					.execPopulate()
+					.populate("event")
+					.populate("user", "-password")
+					.execPopulate();
 
-				//console.log(registration.event.user)
+				registration.owner = registration.event.user;
+				registration.eventTitle = registration.event.title;
+				registration.eventPrice = registration.event.price;
+				registration.eventDate = registration.event.date;
+				registration.userEmail = registration.user.email;
+				registration.save();
 
-				const ownerSocket = req.connectUsers[registration.event.user]
+				const ownerSocket = req.connectUsers[registration.event.user];
 
 				if (ownerSocket) {
-					req.io.to(ownerSocket).emit('registration_request', registration)
+					req.io.to(ownerSocket).emit("registration_request", registration);
 				}
 
-				return res.json(registration)
+				return res.json(registration);
 			}
-		})
+		});
 	},
 
 	async getRegistration(req, res) {
-		const { registration_id } = req.params
+		const { registration_id } = req.params;
 		try {
-			const registration = await Registration.findById(registration_id)
+			const registration = await Registration.findById(registration_id);
 			await registration
-				.populate('event')
-				.populate('user', '-password')
-				.execPopulate()
+				.populate("event")
+				.populate("user", "-password")
+				.execPopulate();
 
-			return res.json(registration)
+			return res.json(registration);
 		} catch (error) {
-			return res.status(400).json({ message: 'Registration not found' })
+			return res.status(400).json({ message: "Registration not found" });
 		}
 	},
-}
+};
